ccb3d97123bbd34030af9ee3cece24f976312516
Anthony Venturini <anthonydventurini@gmail.com>
2019-02-12 18:46:55 -0500

    meets MVP of calculator

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton'
+import ActionButton from './components/ButtonComponents/ActionButton'
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
+
+
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="container">
+      <CalculatorDisplay calculatorDisplay="calc" text="0"/>
+      <div className="flex-container">
+      <ActionButton actionButton="action" text="Clear"/>
+      <NumberButton numberButton="num" text="÷"/>
+      </div>
+      <div className="flex-container">
+      <NumberButton numberButton="num" text="7"/>
+      <NumberButton numberButton="num" text="8"/>
+      <NumberButton numberButton="num" text="9"/>
+      <NumberButton numberButton="ops" text="X"/>
+      </div>
+      <div className="flex-container">
+      <NumberButton numberButton="num" text="4"/>
+      <NumberButton numberButton="num" text="5"/>
+      <NumberButton numberButton="num" text="6"/>
+      <NumberButton numberButton="ops" text="-"/>
+      </div>
+      <div className="flex-container">
+      <NumberButton numberButton="num" text="1"/>
+      <NumberButton numberButton="num" text="2"/>
+      <NumberButton numberButton="num" text="3"/>
+      <NumberButton numberButton="ops" text="+"/>
+      </div>
+      <div className="flex-container">
+      <ActionButton actionButton="action" text="0"/>
+      <NumberButton numberButton="ops" text="÷"/>
+      </div>
     </div>
   );
 };
